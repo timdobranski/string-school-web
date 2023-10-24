e21eb1eac23464e3af07ae70b20e8be4b1de8838
Tim <timdobranski@gmail.com>
2023-10-24 11:51:26 -0700

    Auth flow now works for first time and returning users

--- app/finish-signup/page.js
+++ app/finish-signup/page.js
@@ -10,10 +10,11 @@ export default function Signup() {
   const router = useRouter();
   const [commPref, setCommPref] = useState('none');
   const [preferredEmail, setPreferredEmail] = useState('');
-  const [first_name, setFirstName] = useState('');
-  const [last_name, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [phone, setPhone] = useState('');
   const [authUserId, setAuthUserId] = useState('');
+  const [confirmCode, setConfirmCode] = useState('');
 
 
   useEffect(() => {
@@ -28,6 +29,7 @@ export default function Signup() {
         setLastName(data.session.user.user_metadata.full_name.split(' ')[1]);
         setPhone(data.session.phone);
         setAuthUserId(data.session.user.id);
+        setPreferredEmail(data.session.user.email);
       } else {
         console.log('No session');
       }
@@ -40,24 +42,45 @@ export default function Signup() {
     // console.log('session: ', session);
 
 
-    // Assuming you have the Supabase client set up and imported as 'supabase'
-    const { data, error } = await supabase
-      .from('users')  // Replace with your table name if it's different
-      .insert([
-        {
-          preferred_comm: commPref,
-          preferred_email: preferredEmail,
-          first_name: first_name,
-          last_name: last_name,
-          phone: phone,
-          auth_user_id: authUserId
-        },
-      ]);
+// First, query the students table to find the student with the matching confirmCode
+const { data: studentData, error: studentError } = await supabase
+  .from('students')  // Replace with your students table name if it's different
+  .select('id')
+  .eq('spot_confirm_code', confirmCode)
+  .single();
+
+if (studentError) {
+  console.error('Error getting student:', studentError);
+  return;
+}
+
+// Check if a student with the matching confirmCode was found
+if (!studentData) {
+  console.error('No student found with confirmCode:', confirmCode);
+  return;
+}
+
+// Now, insert the new user with the student_id set to the id of the student found above
+const { data, error } = await supabase
+  .from('users')  // Replace with your users table name if it's different
+  .insert([
+    {
+      preferred_comm: commPref,
+      preferred_email: preferredEmail,
+      first_name: firstName,
+      last_name: lastName,
+      phone: phone,
+      auth_user_id: authUserId,
+      student_id: studentData.id,  // Use the id of the student found above
+    },
+  ]);
 
     if (error) {
-      console.log('Error:', error);
-    } else {
-      console.log('Data:', data);
+      console.error('Error inserting user:', error);
+      return;
+    }
+    else {
+      console.log('User inserted:', data);
       router.push('/students/home')
     }
   };
@@ -86,7 +109,26 @@ return (
           </select>
         </label>
       </div>
-
+      <div>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+      </div>
       <div>
         <label>
           Preferred Email:
@@ -94,6 +136,27 @@ return (
             type="text"
             value={preferredEmail}
             onChange={(e) => setPreferredEmail(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Phone:
+          <input
+            type="text"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          If you are a preexisting student, enter your confirmation code to associate your
+          account with your student:
+          <input
+            type="text"
+            value={confirmCode}
+            onChange={(e) => setConfirmCode(e.target.value)}
           />
         </label>
       </div>
