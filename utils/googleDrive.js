5bc9c7060b3040bc0486198d38047a23949f9111
Tim <timdobranski@gmail.com>
2023-11-21 22:44:58 -0800

    Refactored google drive api helper to use env variables for production

--- utils/googleDrive.js
+++ utils/googleDrive.js
@@ -3,10 +3,22 @@ import path from 'path';
 
 const keyFilePath = path.join(process.cwd(), 'credentials.json');
 
+const googleAuthConfig = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+};
 // authenticates the service account to be used in this context
 const auth = new google.auth.GoogleAuth({
     // your credentials to authenticate
-    keyFile: keyFilePath,
+    credentials: googleAuthConfig,
     // the actions you are permissed to perform using this API, in this case
     // all CRUD operations are permissed, check out
     // [ https://developers.google.com/drive/api/guides/api-specific-auth ]
