fb67de8b793c84f115e0e882c58d49325667537e
Tim <timdobranski@gmail.com>
2023-12-02 21:07:38 -0800

    Added total row to bottom of skills table

--- app/students/progress/skills/page.js
+++ app/students/progress/skills/page.js
@@ -18,14 +18,16 @@ export default function Skills() {
         const fetchedSkills = await getSkills(student.id);
         console.log('skills: ', fetchedSkills);
         setSkills(fetchedSkills);
-      };
+      };``
       loadSkills();
     }
   }, [student]);
 
-  // get student's skills
+  // render the table of skills
   const renderSkillsTable = () => {
+
     const filteredSkills = skills.filter(skill => filterType === 'all' || skill[`${filterType}_level`] > 0);
+    const { totalUnderstanding, totalPlaying, totalEar, maxUnderstanding, maxPlaying, maxEar } = calculateTotals(filteredSkills);
 
     return (
       <table className={styles.skillsTable}>
@@ -35,7 +37,7 @@ export default function Skills() {
             {filterType === 'all' && <>
               <th className={`${styles.understandingColumn} ${styles.headerRow}`}>Understanding</th>
               <th className={`${styles.playingColumn} ${styles.headerRow}`}>Playing</th>
-              <th className={`${styles.earColumn} ${styles.headerRow}`}>Ear</th>
+              <th className={`${styles.earColumn} ${styles.headerRow}`}>Ear Training</th>
             </>}
             {filterType !== 'all' && <th className={styles.specificSkillColumn}>{filterType.charAt(0).toUpperCase() + filterType.slice(1)}</th>}
           </tr>
@@ -52,10 +54,47 @@ export default function Skills() {
               {filterType !== 'all' && <td className={styles.specificSkillColumn}>{`${skill[`${filterType}_level`]}/3`}</td>}
             </tr>
           ))}
+          <tr className={styles.totalRow}>
+            <td className={`${styles.skillNameColumn} ${styles.totalCell}`}>Total</td>
+            {filterType === 'all' && <>
+              <td className={`${styles.understandingColumn} ${styles.totalCell}`}>{`${totalUnderstanding}/${maxUnderstanding}`}</td>
+              <td className={`${styles.playingColumn} ${styles.totalCell}`}>{`${totalPlaying}/${maxPlaying}`}</td>
+              <td className={`${styles.earColumn} ${styles.totalCell}`}>{`${totalEar}/${maxEar}`}</td>
+            </>}
+            {filterType !== 'all' && <td className={`${styles.specificSkillColumn} ${styles.totalCell}`}>{`${eval('total' + filterType.charAt(0).toUpperCase() + filterType.slice(1))}/${eval('max' + filterType.charAt(0).toUpperCase() + filterType.slice(1))}`}</td>}
+          </tr>
         </tbody>
       </table>
     );
   }
+
+  const calculateTotals = (skills) => {
+    let totalUnderstanding = 0;
+    let totalPlaying = 0;
+    let totalEar = 0;
+    let maxUnderstanding = 0;
+    let maxPlaying = 0;
+    let maxEar = 0;
+
+    skills.forEach(skill => {
+      if (skill.understanding_test) {
+        totalUnderstanding += skill.understanding_level;
+        maxUnderstanding += 3; // Add 3 to the maximum possible score
+      }
+      if (skill.playing_test) {
+        totalPlaying += skill.playing_level;
+        maxPlaying += 3;
+      }
+      if (skill.ear_test) {
+        totalEar += skill.ear_level;
+        maxEar += 3;
+      }
+    });
+
+    return { totalUnderstanding, totalPlaying, totalEar, maxUnderstanding, maxPlaying, maxEar };
+  };
+
+
 
   return (
     <div className='infoCard'>
