6af5afddf9fd0a66b764788afd35ad214f3e7379
Tim <timdobranski@gmail.com>
2024-11-05 16:30:50 -0800

    Styling updates in about lessonst push origin main

--- app/public/about-lessons/page.js
+++ app/public/about-lessons/page.js
@@ -2,44 +2,64 @@ import styles from './about-lessons.module.css'
 
 export default function AboutLessons() {
   return (
-    <div className='infoCard'>
-      <h2 className='featureHeaders'>Lesson Schedule</h2>
-      <p className='text'>{`Lessons are offered in 30 minute recurring weekly blocks at the following times:`}</p>
-      <table className={styles.scheduleTable}>
-        <tr>
-          <td className={styles.days}>Mon-Thurs</td>
-          <td className={styles.times}>4:30pm - 8:30pm</td>
-        </tr>
-        <tr>
-          <td className={styles.days}>Fri</td>
-          <td className={styles.times}>4:30pm - 6:00pm</td>
-        </tr>
-        <tr>
-          <td className={styles.days}>Sun</td>
-          <td className={styles.times}>10:00am - 12:00pm</td>
-        </tr>
-      </table>
-      <p className='text'>{`Students interested in longer or more frequent lessons are welcome to book multiple lessons.`}</p>
+    <div className='pageContentWrapper'>
+      <div className={styles.lessonInfoWrapper}>
 
-
-      <h2 className='featureHeaders'>Lesson Fees</h2>
-      <p className='text'>Fees are the same every month, regardless of the number of weeks/lessons in the month.</p>
-      <p className='text'>Most months will include 4 weekly lessons; some will include an extra 5th.</p>
-      <p className='text'></p>
-
-      <div className={styles.feeDivider}>
-        <div className={styles.feesLeft}>
-          <p className={styles.price}>$120/month</p>
-          <p>Cash</p>
+        <div className={styles.lessonInfoSection}>
+          <h2 className='featureHeaders'>When</h2>
+          <p className='text'>{`Lessons are offered in 30 minute recurring weekly blocks at the following times:`}</p>
+          <table className={styles.scheduleTable}>
+            <tr>
+              <td className={styles.days}>Mon-Thurs</td>
+              <td className={styles.times}>4:30pm - 8:30pm</td>
+            </tr>
+            <tr>
+              <td className={styles.days}>Fri</td>
+              <td className={styles.times}>4:30pm - 6:00pm</td>
+            </tr>
+            <tr>
+              <td className={styles.days}>Sun</td>
+              <td className={styles.times}>10:00am - 12:00pm</td>
+            </tr>
+          </table>
+          <p className='text'>{`Students interested in longer or more frequent lessons are welcome to book multiple lessons.`}</p>
         </div>
-        <div className={styles.feesColumn}>
+
+        <div className={`${styles.lessonInfoSection} ${styles.middleInfoSection}`}>
+          <div className={styles.verticalDividerLeft}></div>
+          <h2 className='featureHeaders'>Where</h2>
+          <div className={styles.verticalDividerRight}></div>
+          <p className='text'>{`Lessons are held at my home in Fletcher Hills, CA. As this is my personal residence as well,
+            the address is not listed here and is provided upon booking a lesson.`} </p>
+
+<p className='text'>{`I also offer lessons online via Zoom. These lessons can be much more convenient, but I don't recommend them
+for all situations. Feel free to reach out if this is something you'd like to discuss!`}
+          </p>
+        </div>
+
+        <div className={styles.lessonInfoSection}>
+
+          <h2 className='featureHeaders'>Fees</h2>
+          <p className='text'>Fees are the same every month, regardless of the number of weeks/lessons in the month.</p>
+          <p className='text'>Most months will include 4 weekly lessons; some will include an extra 5th.</p>
+          <p className='text'></p>
+
+          <div className={styles.feeDivider}>
+            <div className={styles.feesLeft}>
+              <p className={styles.price}>$120/month</p>
+              <p>Cash</p>
+            </div>
+            <div className={styles.feesColumn}>
           or
+            </div>
+            <div className={styles.feesRight}>
+              <p className={styles.price}>$140/month</p>
+              <p>Venmo</p>
+              <p>Paypal</p>
+            </div>
+          </div>
         </div>
-        <div className={styles.feesRight}>
-          <p className={styles.price}>$140/month</p>
-          <p>Venmo</p>
-          <p>Paypal</p>
-        </div>
+
       </div>
 
       <h2 className='featureHeaders'>Lesson Policies</h2>
